c5173bf68f33796cb0fd3aecec03f61576dd2480
taiwoj121 <taiwoj121@gmail.com>
2024-04-18 21:11:45 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return ( 
+    <div>
+      <h1 className="underline">I am taiwo</h1>
+      <p className="bg">This is working</p>
+    </div>
+   );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    colors: {
+      'bgColor':'#EFF5FF',
+      'bg-b': '#D6D9E6',
+      'light-blue': '#ABBCFF',
+      'denim': '#022959',
+      'light-grey': '#D6D9E6',
+      'orange': '#FFAF7E',
+      'pink': '#F9818E',
+      'purple': '#483EFF',
+      'red': '#EE374A',
+      'sky-blue': '#BEE2FD',
+      'grey-00': '#F8F9FF',
+      'white': '#FFFF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
