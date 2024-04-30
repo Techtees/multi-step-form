660e2fb88d7beadea74bbec7e8c563ed79137396
taiwoj121 <taiwoj121@gmail.com>
2024-04-30 17:29:09 +0100

    Style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import SideBar from "./components/Aside/SideBar";
+import StepLogic from "./components/StepLogic";
 function App() {
   return ( 
-    <div>
-      <h1 className="underline">I am taiwo</h1>
-      <p className="bg">This is working</p>
+    <div className="overflow-hidden lg:h-screen flex items-center justify-center">
+       <div className="lg:bg-white w-full lg:w-[940px] lg:p-5 lg:shadow-md lg:rounded-lg lg:flex lg:justify-between">
+           <SideBar />
+           <StepLogic />
+       </div>
     </div>
    );
 }
--- src/utils/Inputs/RadioInput.tsx
+++ src/utils/Inputs/RadioInput.tsx
@@ -14,7 +14,7 @@ type RadioProps = {
 export const RadioInput = ({ name, id, info, checked, icon, title, price} : RadioProps) => {
     return(
         <>
-            <label className="w-full h-[180px]">
+            <label className="w-full lg:h-[180px]">
                 <input
                     name={name}
                     id={id}
@@ -22,7 +22,7 @@ export const RadioInput = ({ name, id, info, checked, icon, title, price} : Radi
                     className=" peer hidden " 
                     checked={checked}
                 />
-                <div className="flex cursor-pointer  flex-col justify-between w-full h-full rounded-lg border-borderColor p-5 border hover:ring-1 hover:ring-purple peer-checked:ring-1 peer-checked: ring-purple ">
+                <div className="flex gap-3 lg:gap-0 cursor-pointer  lg:flex-col lg:justify-between w-full h-fit lg:h-full rounded-lg border-borderColor p-3 lg:p-5 border hover:ring-1 hover:ring-purple peer-checked:ring-1 peer-checked: ring-purple ">
                     {icon}
                     <div className="flex flex-col">
                         <span className="text-lg mb-1 font-semibold text-denim">{title}</span>
