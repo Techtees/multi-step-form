4a1fc4fbd0db48989d6dbe6ca7d709590db36d28
taiwoj121 <taiwoj121@gmail.com>
2024-05-14 18:09:54 +0100

    updated global types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,32 @@
-type StepDataType = {
+export type StepDataType = {
     stepNo: number;
     stepTitle: string;
 }
+
+type InputType = {
+    name: string,
+    label: string,
+    type: string,
+    placeholder: string,
+    value: string,
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+    error?: string ;
+    validate?: (inputValue: string) => string | undefined;
+}
+
+interface Package {
+    packageName: string,
+    packagePrice: string,
+}
+interface Plan  {
+    package: Pacakge,
+    planType: string,
+}
+
+export type FormData = {
+    fullName: string,
+    email: string,
+    phone: string,
+    plan: Plan;
+}
+
