1871dd02ce17368542a940f3576c3197a4de5713
taiwoj121 <taiwoj121@gmail.com>
2024-05-17 23:31:20 +0100

    style changes: updated button color

--- src/utils/Button/index.tsx
+++ src/utils/Button/index.tsx
@@ -11,7 +11,7 @@ export const Button = ({text, onClick, type, btnType} : ButtonProps) => {
         <button
             onClick={onClick}
             type= {type}
-            className={`h-[40px] rounded-lg border font-medium pl-5 pr-5 flex text-sm justify-center items-center ${btnType === 'primary' && 'border-none pl-0 pr-0 text-grey hover:text-denim'} ${btnType=== 'secondary' && 'bg-denim text-white'} ${btnType === 'tertiary' && 'bg-purple border-none text-white'}`}
+            className={`h-[40px] rounded-lg border font-medium pl-5 pr-5 flex text-sm justify-center items-center ${btnType === 'primary' && 'border-none pl-0 pr-0 text-grey ] hover:text-denim'} ${btnType=== 'secondary' && 'bg-denim text-white hover:bg-[#164A8A]'} ${btnType === 'tertiary' && 'bg-purple border-none text-white'}`}
         > {text} 
         </button>
     )
