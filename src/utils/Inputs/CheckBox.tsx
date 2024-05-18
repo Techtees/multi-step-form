b4fefb84a7a0a4e345d98cda4db90deb56dbf31e
taiwoj121 <taiwoj121@gmail.com>
2024-05-18 22:52:10 +0100

    checkbox type fix

--- src/utils/Inputs/CheckBox.tsx
+++ src/utils/Inputs/CheckBox.tsx
@@ -3,24 +3,30 @@ type CheckBoxProps = {
     title: string;
     info: string;
     price: string;
-    name?: string
+    name: string;
+    value: string;
+    isChecked?: boolean;
+    onChange:(e: React.ChangeEvent<HTMLInputElement>) => void;
 
 }
 
-export const CheckBox = ({id, title, info, price} : CheckBoxProps) => {
+export const CheckBox = ({id, title, info, price, isChecked, onChange, value, name} : CheckBoxProps) => {
     return(
         <>
             <div>
                 <label htmlFor={id}>
                     <input type="checkbox"
                         id={id}
-                        name={name}
+                        name={name} //eslint-disable-line
                         className="peer hidden"
+                        onChange={onChange}
+                        checked = {isChecked}
+                        value={value}
                     />
                     <div className="border peer-checked:ring-1 peer-checked:ring-purple rounded-lg border-borderColor cursor-pointer flex  items-center justify-between p-3 " >
                         <div className="flex items-center space-x-4">
-                            <div className="h-[18px] w-[18px] rounded border border-borderColor flex justify-center items-center p-1 ">
-                                <div className="h-[10px] w-[5px] border-r-2 border-borderColor border-b-2 rotate-45 mb-[2px]"> </div>
+                            <div className={`h-[18px] w-[18px] rounded border border-borderColor flex justify-center items-center p-1 ${isChecked ? 'bg-purple border-none': ''}`}>
+                                <div className={`h-[10px] w-[5px] border-r-2 border-borderColor border-b-2 rotate-45 mb-[2px] ${isChecked ? '' :'hidden'}`}> </div>
                             </div>
                             <div>
                                 <p className="text-denim font-bold">{title}</p>
